920b97bc409e61bde489d0a4a8cde3f6e68db5d2
akicho8 <akicho8@gmail.com>
2023-03-11 18:14:45 +0900

    [chore] add 0000_invert_table.js

---
+++ examples/0000_invert_table.js
@@ -0,0 +1,24 @@
+import MemoryRecord from '../src/memory_record.js'
+
+class Fruit extends MemoryRecord {
+  static get define() {
+    return [
+      { key: "a",  name: "alice", },
+      { key: "b",  name: "bob",   },
+    ]
+  }
+
+  static lookup(key) {
+    return super.lookup(key) || this.invert_table[key]
+  }
+
+  static get invert_table() {
+    if (this._invert_table != null) {
+      return this._invert_table
+    }
+    this._invert_table = this.memory_record_create_index_or(["name"])
+    return this._invert_table
+  }
+}
+
+console.log(Fruit.fetch("alice").key)
